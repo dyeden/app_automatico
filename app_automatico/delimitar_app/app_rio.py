3eea9cad9b300a3b953b4a08d98c650bd4b57a97
dyeden <dyedenm@gmail.com>
2015-04-24 18:09:29 -0300

    apps

---
+++ app_automatico/delimitar_app/app_rio.py
@@ -0,0 +1,9 @@
+class AppRio():
+    def __init__(self):
+        self.dict_poligono_descricao = None
+    def analisar_linhas(self):
+        for linha in self.dict_poligono_descricao["metadados"]["linhas"]:
+
+            pass
+    def iniciar_codigo(self):
+        self.analisar_linhas()
