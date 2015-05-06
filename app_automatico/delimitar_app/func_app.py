da2509d051c6b0cfff4169a6f70de90ea02ee019
dyeden <dyedenm@gmail.com>
2015-05-06 00:56:31 -0300

    editando poligono app

--- app_automatico/delimitar_app/func_app.py
+++ app_automatico/delimitar_app/func_app.py
@@ -5,10 +5,19 @@ poligono_ma = None
 borda_linha_geo = None
 borda_linha_plana = None
 from func_circulo import CircVetores
-from arcpy import PointGeometry
-def criar_linha_largura_app(linha):
+from arcpy import Point, PointGeometry, Polyline, Polygon, Array
+def definir_largura_app(linha):
     compri_linha = linha.projectAs(projecao_plana).length
-    largura_app = 50
+    compri_app = None
+    if compri_linha < 10:
+        compri_app = 30
+    elif compri_app <= 10 and compri_app < 50:
+        compri_app = 50
+    elif compri_app <= 50 and compri_app < 200:
+        compri_app = 100
+    return compri_app
+
+def criar_linha_largura_app(linha, largura_app):
     x_1_c = linha.firstPoint.X
     y_1_c = linha.firstPoint.Y
     x_2_c = linha.lastPoint.X
@@ -32,14 +41,28 @@ def criar_linha_largura_app(linha):
     angulo_rad = obj_circ.retorna_angulo_atraves_ponto(x_1_c,y_1_c)
     x_2_final, y_2_final = obj_circ.retorna_ponto_de_angulo_inverso(angulo_rad, raio)
     del obj_circ
-    #TODO finalizar a funcao
-
-
+    point1_final = Point()
+    point1_final.X = x_1_final
+    point1_final.Y = y_1_final
+    point2_final = Point()
+    point2_final.X = x_2_final
+    point2_final.Y = y_2_final
+    array = Array([point1_final, point2_final])
+    linha_app = Polyline(array, projecao_geo)
+    del array
+    return linha_app
 
 def criar_poligono_app(linha, linha_frente):
-    compri_linha = linha.projectAs(projecao_plana).length
-    compri_linha_frente = linha_frente.projectAs(projecao_plana).length
-    criar_linha_largura_app(linha)
-    # compri_app_linha = identificar_largura_app(compri_linha)
-    # compri_app_linha_frente = identificar_largura_app(compri_linha_frente)
-    # circ_linha =
+    linha_app = criar_linha_largura_app(linha, definir_largura_app(linha))
+    linha_app_frente =  criar_linha_largura_app(linha_frente, definir_largura_app(linha_frente))
+    array = Array()
+    array.add(linha_app.firstPoint)
+    array.add(linha_app.lastPoint)
+    array.add(linha_app_frente.lastPoint)
+    array.add(linha_app_frente.firstPoint)
+    array.add(linha_app.firstPoint)
+    polygon = Polygon(array, projecao_geo)
+    array.removeAll()
+    del array
+    return polygon
+
