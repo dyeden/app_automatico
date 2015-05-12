1905da772a4ecea5821943f562e6048db8553d8f
dyeden <dyedenm@gmail.com>
2015-05-12 18:24:03 -0300

    cruzamento de linhas

--- app_automatico/delimitar_app/func_app.py
+++ app_automatico/delimitar_app/func_app.py
@@ -6,6 +6,7 @@ borda_linha_geo = None
 borda_linha_plana = None
 from func_circulo import CircVetores
 from arcpy import Point, PointGeometry, Polyline, Polygon, Array
+import numpy
 def definir_largura_app(linha):
     compri_linha = linha.projectAs(projecao_plana).length
     compri_app = None
@@ -16,6 +17,37 @@ def definir_largura_app(linha):
     elif compri_app <= 50 and compri_app < 200:
         compri_app = 100
     return compri_app
+
+def bool_interseccao_entre_linhas(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       return False
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    l1_minX = min(line1[0][0], line1[1][0])
+    l1_maxX = max(line1[0][0], line1[1][0])
+    l2_minX = min(line2[0][0], line2[1][0])
+    l2_maxX = max(line2[0][0], line2[1][0])
+    l1_minY = min(line1[0][1], line1[1][1])
+    l1_maxY = max(line1[0][1], line1[1][1])
+    l2_minY = min(line2[0][1], line2[1][1])
+    l2_maxY = max(line2[0][1], line2[1][1])
+
+    if l1_minX <= x <= l1_maxX and l2_minX <= x <= l2_maxX:
+        if l1_minY <= y <= l1_maxY and l2_minY <= y <= l2_maxY:
+            return True
+        else:
+            return False
+    else:
+        return False
+
 
 def criar_linha_largura_app(linha, largura_app):
     x_1_c = linha.firstPoint.X
@@ -56,9 +88,20 @@ def criar_poligono_app(linha, linha_frente):
     linha_app = criar_linha_largura_app(linha, definir_largura_app(linha))
     linha_app_frente =  criar_linha_largura_app(linha_frente, definir_largura_app(linha_frente))
     array = Array()
+
+    l1_firstX = linha_app.firstPoint.X
+    l1_firstY = linha_app.firstPoint.Y
+    l1_lastX = linha_app.lastPoint.X
+    l1_lastY = linha_app.lastPoint.Y
+    l2_firstX = linha_app_frente.firstPoint.X
+    l2_firstY = linha_app_frente.firstPoint.Y
+    l2_lastX = linha_app_frente.lastPoint.X
+    l2_lastY = linha_app_frente.lastPoint.Y
     array.add(linha_app.firstPoint)
     array.add(linha_app.lastPoint)
     array.add(linha_app_frente.lastPoint)
+
+
     array.add(linha_app_frente.firstPoint)
     array.add(linha_app.firstPoint)
     polygon = Polygon(array, projecao_geo)
